ae994bc72e14ac3aff23c6f3f4ae5b4ad07cc238
vrobertomiranda <vrobertomiranda@gmail.com>
2023-08-10 18:07:58 -0300

    Aula 3

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -102,4 +102,22 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success').should('be.visible')         // commands.js dentro do projeto de Curso Cypress Básico  
     })
 
+    it('Seleciona um produto (YouTube) por seu texto', function() {
+        cy.get('#product')
+          .select('YouTube')
+          .should('have.value', 'youtube') 
+    })
+
+    it('Seleciona um produto (Mentoria) por seu valor (value)', function() {
+        cy.get('#product')
+        .select('mentoria')
+        .should('have.value', 'mentoria') 
+    })
+
+    it.only('Seleciona um produto (Blog) por seu índice', function() {
+        cy.get('#product')
+        .select(1)
+        .should('have.value', 'blog') 
+    })
+
 })
